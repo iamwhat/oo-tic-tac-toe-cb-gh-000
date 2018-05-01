41b1c2cf709d21e770438ec78f1213bffe46f79f
William Li <githuberr@outlook.com>
2018-05-01 10:49:01 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,4 +50,14 @@ class TicTacToe
     end
   end
 
+  def turn_count
+    counter = 0
+    @board.each do |item|
+      if item=="X" or item=="O"
+        counter += 1
+      end
+    end
+    counter
+  end  
+
 end
